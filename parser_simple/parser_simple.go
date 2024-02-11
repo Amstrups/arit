2f36315d51361c2d172115b87c1dc848655df91d
Christian Amstrup Petersen <christian.amstrup@hotmail.com>
2024-02-11 22:35:22 +0100

    start lalr(1) parsing table greedy implementation

--- parser_simple/parser_simple.go
+++ parser_simple/parser_simple.go
@@ -1,33 +1,79 @@
 package parsersimple
 
 import (
-  "arit/lexer_simple"
+	l "arit/lexer_simple"
   "fmt"
 )
-type Lexed struct {
-  pos lexersimple.Position
-  tok lexersimple.Token
+
+
+type EnrichedToken struct {
+  pos l.Position
+  tok l.Token
   s string
 }
 
-type buffer struct {
-
-
+type state struct {
+  current int 
+  next int
 }
-
-func (b *buffer) pop() 
-
 
 type Parser struct { 
-  ch chan Lexed 
+  lexer l.GLexer
+  exps []Exp 
+  states []int
+  lookahead *EnrichedToken
 }
 
-func newParser() *Parser {
-  return &Parser{make(chan Lexed)}
-}
-
-func (p *Parser) Parse() {
-  for i := range p.ch {
-
+func NewParser(l l.GLexer) *Parser {
+  return &Parser{
+    lexer: l,
+    exps: []Exp{},
+    states: []int{0}, 
   }
+}
+
+func (p *Parser) popState() {
+  p.states = p.states[:len(p.states)-2] 
+}
+
+func (p *Parser) reduce() {
+  
+}
+
+func (p *Parser) shift() {  
+    pos, t, s := p.lexer.Lex()
+    p.lookahead = &EnrichedToken{pos,t,s}
+}
+
+func (p *Parser) parse() {
+  
+}
+
+func (p *Parser) goTo() { 
+  switch state := p.states[len(p.states)-1]; state { 
+  case 0:
+    p.states = append(p.states, 1)
+  case 3:
+    p.states = append(p.states, 9)
+  case 4:
+    p.states = append(p.states, 10)
+  case 5:
+    p.states = append(p.states, 11)
+  case 6:
+    p.states = append(p.states, 12)
+  case 7:
+    p.states = append(p.states, 13)
+  case 8:
+    p.states = append(p.states, 14)
+  default:
+    fmt.Println("default")
+  }
+}
+
+func (p *Parser) Parse() Exp {
+  p.parse()
+  if (len(p.exps) > 1) {
+    panic("fuck")
+  }
+  return p.exps[0]
 }
