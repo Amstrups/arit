a8d8255fdca470d54dfb679c53ab7f2f2d9612eb
Christian Amstrup Petersen <christian.amstrup@hotmail.com>
2024-02-11 22:32:44 +0100

    remove channel testing

--- main.go
+++ main.go
@@ -1,42 +1,16 @@
 package main
 
 import (
-  "arit/lexer_simple"
+	lexer "arit/lexer_simple"
+  parser "arit/parser_simple"
 	"fmt"
 )
 
-
-func f(c chan int) {
-  x := []int{1,2,3,4,5,6,7,8,9,0}
-  for i, v := range x {
-    if i % 3 == 0 {
-      c <- v
-    }
-  }
-  close(c)
-
-}
-
-func channelTesting() { 
-  ch := make(chan int)
-  go f(ch)
-  for i := range ch {
-      fmt.Println(i) 
-      
-  }
-}
-
 func main() { 
-  channelTesting()
-  return
-  l := lexersimple.NewSLexer("+-*/\n111.111")
-  for { 
-    p,t,s := l.Lex()
-    if t == lexersimple.EOF {
-      return
-    }
-    fmt.Printf("Position: %d:%d, Token: %d, String: %s\n", p.Line, p.Column, t, s)
-  }
+  l := lexer.NewSLexer("+-*/\n111.111")
+  p := parser.NewParser(l)
+  stmt := p.Parse()
+  fmt.Println(stmt)
 }
 
 /* Scanner example
